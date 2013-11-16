8fc5b41c94abdae4cb413effe8409d1faf2b2c92
Pedro José Piquero Plaza <gowikel@gmail.com>
2013-11-16 18:39:29 +0100

    Added the following function prototypes to the class UnsharpMask
    - setCutOffFrequency
    - setOrderButterworth
    - setGain
    - getCutOffFrequency
    - getOrderButterworth
    - get Gain
    - Default constructor

---
+++ UnsharpMask.hpp
@@ -0,0 +1,49 @@
+#ifndef __UNSHARP_MASK__
+#define __UNSHARP_MASK__
+
+#include <opencv2/core/core.hpp>
+
+namespace FSIV
+{
+  class UnsharpMask
+  {
+  private:
+    double _cutoffFrequency; //!<Cutoff frequency
+    unsigned short int _orderButterworth; //!< Order Butterworth filter
+    double _gain; //!< Gain enhancement
+
+  public:
+    /** @brief Default Constructor
+	@param cutoff Cutoff frequency, default 0.5
+	@param orderButterWorth Order ButterWorth filter, default 2
+	@param gain, Gain enhancement, default 1
+    **/
+    inline UnsharpMask(double cutoff = 0.5, unsigned short int orderButterWorth = 2, double gain = 1);
+
+    /** @brief Sets cutoff frequency
+	@param cutoff Cutoff frequency
+    **/
+    void setCutOffFrequency(double cutoff);
+
+    /** @brief Sets order Butterworth filter 
+	@param order Order butterworth filter
+    **/
+    void setOrderButterworth(unsigned short int order);
+
+    /** @brief Sets gain enhancement
+	@param gain Gain enhancement
+    **/
+    void setGain(double gain);
+
+    /** @brief Returns cutoff frequency saved **/
+    double getCutOffFrequency() const;
+
+    /** @brief Returns butterworth order **/
+    unsigned short int getOrderButterworth() const;
+
+    /** @brief Returns gain enhancement **/
+    double getGain();
+  }
+}
+
+#endif
