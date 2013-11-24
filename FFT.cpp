49d2b8be18342aba6887cc6eb888fd490bc5b5bf
Pedro José Piquero Plaza <gowikel@gmail.com>
2013-11-24 20:17:57 +0100

    Implemented FFT:inverseFFT

--- FFT.cpp
+++ FFT.cpp
@@ -1,4 +1,5 @@
 #include <opencv2/core/core.hpp>
+#include <cassert>
 #include "FFT.hpp"
 
 FFT::FFT(const Mat &picture = Mat())
@@ -17,3 +18,69 @@ void FFT::setPicture(const Mat &picture = Mat())
 	_picture = picture.clone();
 	_fft.release();
 }
+
+void FFT::doFFT()
+{
+	assert(!_picture.empty());
+	Mat picture = this->getPicture();
+	Mat fft;
+	
+	if(picture.channels() == 1)
+	{
+		picture.convertTo(fft, CV_32F);
+		dft(fft, fft, cv::DFT_SCALE|cv::DFT_COMPLEX_OUTPUT);
+
+		int cx = fft.cols/2;
+		int cy = fft.rows/2;
+
+		Mat q0(fft, Rect(0, 0, cx, cy));   // Top-Left
+		Mat q1(fft, Rect(cx, 0, cx, cy));  // Top-Right
+		Mat q2(fft, Rect(0, cy, cx, cy));  // Bottom-Left
+		Mat q3(fft, Rect(cx, cy, cx, cy)); // Bottom-Right
+
+		Mat tmp; 
+		q0.copyTo(tmp);
+		q3.copyTo(q0);
+		tmp.copyTo(q3);
+
+		q1.copyTo(tmp);
+		q2.copyTo(q1);
+		tmp.copyTo(q2);
+		
+		_fft = fft.clone();
+	}
+}
+
+void FFT::inverseFFT()
+{
+	assert(!_fft.empty());
+	
+	Mat fft = this->getFFT();
+	Mat fftCopy = fft.clone();
+	
+	if(_picture.channels() == 1)
+	{
+		int cx = fft.cols/2;
+		int cy = fft.rows/2;
+
+		Mat q0(fft, Rect(0, 0, cx, cy));   // Top-Left
+		Mat q1(fft, Rect(cx, 0, cx, cy));  // Top-Right
+		Mat q2(fft, Rect(0, cy, cx, cy));  // Bottom-Left
+		Mat q3(fft, Rect(cx, cy, cx, cy)); // Bottom-Right
+
+		Mat tmp;
+		q0.copyTo(tmp);
+		q3.copyTo(q0);
+		tmp.copyTo(q3);
+
+		q1.copyTo(tmp);
+		q2.copyTo(q1);
+		tmp.copyTo(q2);
+
+		dft(fft, fft, cv::DFT_INVERSE|cv::DFT_REAL_OUTPUT);
+		fft.convertTo(result, CV_8U);
+		this->setPicture(fft);
+		_fft = fftCopy.clone();
+	}
+}
+
