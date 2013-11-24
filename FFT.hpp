5182eeccd2618d894dcd68f5ad14234f2d7ab05d
Pedro José Piquero Plaza <gowikel@gmail.com>
2013-11-24 19:17:17 +0100

    Created FFT class. It will do fourier transform and save it

---
+++ FFT.hpp
@@ -0,0 +1,56 @@
+#ifndef __FFT_HPP__FSIV
+#define __FFT_HPP__FSIV
+
+#include <opencv2/core/core.hpp>
+
+using namespace cv;
+
+class FFT
+{
+public:
+	/** 
+	 * @brief Default constructor
+	 * @param picture Picture to be transformed
+	 * **/
+	FFT(const Mat &picture = Mat());
+	
+	/** @brief Copy Constructor
+	 *  @param f FFT to be copied
+	
+	/**
+	 * @brief Set up a picture to be transformed
+	 * @param picture Picture to be transformed
+	 * **/
+	void setPicture(const Mat &picture = Mat());
+	
+	/**
+	 * @brief Do transformation
+	 * **/
+	void doFFT();
+	
+	/**
+	 * @brief Do inverse FFT
+	 * **/
+	void inverseFFT();
+	
+	/**
+	 * @brief Show picture's fft
+	 * **/
+	void showFFT();
+	
+	/**
+	 * @brief Return fft's copy
+	 * **/
+	Mat getFFT();
+	
+	/**
+	 * @brief Return picture
+	 * **/
+	Mat getPicture();
+	
+private:
+	Mat _picture;
+	Mat _fft;
+};
+
+#endif
